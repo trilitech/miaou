bbacfe56981aeccdad9239391a6fd05c86a87b0c
Mathias Bourgoin <mathias.bourgoin@gmail.com>
2026-01-12 21:33:26 +0100

    Fix narrow_modal key_binding alias

--- src/miaou_core/narrow_modal.ml
+++ src/miaou_core/narrow_modal.ml
@@ -12,7 +12,12 @@ module Page : PAGE_SIG = struct
 
   type msg = unit
 
-  type key_binding = Tui_page.key_binding
+  type key_binding = {
+    key : string;
+    action : pstate -> pstate;
+    help : string;
+    display_only : bool;
+  }
 
   type pstate = state Navigation.t
 
